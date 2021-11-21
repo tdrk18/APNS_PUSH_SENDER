d9dced2cefab8aa4456ee541c4a226bbcae489c4
tdrk18 <s18.todoroki@gmail.com>
2021-11-21 21:47:16 +0900

    Add send_push.sh

---
+++ send_push.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+BUNDLE_IDENTIFIER=""
+TOKEN=""
+
+while getopts ":-:" opt; do
+  if [ $opt = "-" ]; then
+    opt=`echo ${OPTARG} | awk -F'=' '{print $1}'`
+    OPTARG=`echo ${OPTARG} | awk -F'=' '{print $2}'`
+  fi
+
+  case "$opt" in
+    bundleID)
+      BUNDLE_IDENTIFIER=$OPTARG
+      ;;
+    token)
+      TOKEN=$OPTARG
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+PUSH_DATA='{"aps": {"alert": "🎉push test"}}'
+APPLE_URI="https://api.push.apple.com/3/device/"
+PEM_PATH="/var/tmp/keys/certificate-and-privatekey.pem"
+
+curl -v \
+  -d "$PUSH_DATA" \
+  -H "apns-priority: 10" \
+  -H "apns-expiration: 0" \
+  -H "apns-topic: ${BUNDLE_IDENTIFIER}" \
+  --http2 \
+  --cert ${PEM_PATH} \
+  ${APPLE_URI}${TOKEN}
