bd892b239e85bff7b001e36ed86a3703939fa46d
tdrk18 <tdrk18@gmail.com>
2021-11-29 12:37:48 +0900

    Create pem file from p12 file

--- send_push.sh
+++ send_push.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+P12_PASSWORD=""
 BUNDLE_IDENTIFIER=""
 TOKEN=""
 
@@ -16,6 +17,9 @@ while getopts ":-:" opt; do
     token)
       TOKEN=$OPTARG
       ;;
+    password)
+      P12_PASSWORD=$OPTARG
+      ;;
   esac
 done
 shift $((OPTIND - 1))
@@ -23,6 +27,9 @@ shift $((OPTIND - 1))
 PUSH_DATA='{"aps": {"alert": "🎉push test"}}'
 APPLE_URI="https://api.push.apple.com/3/device/"
 PEM_PATH="/var/tmp/keys/certificate-and-privatekey.pem"
+P12_PATH="/var/tmp/keys/aps_key.p12"
+
+openssl pkcs12 -password pass:${P12_PASSWORD} -in ${P12_PATH} -nodes -out ${PEM_PATH}
 
 curl -v \
   -d "$PUSH_DATA" \
